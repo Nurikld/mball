572daf2cb71b473103531675003039db7dbebd57
nuriakgd@gmail.com <nuria>
2023-03-31 17:14:57 +0200

    initial commit

---
+++ mball.rb
@@ -0,0 +1,33 @@
+
+greetings = [
+  "Hello, dear friend. Just answering your question...",
+  "Whoever asks a question will get an answer...",
+  "Hi, Mortal One. This is your answer today."
+]
+
+answers = [
+  "Undoubtedly",
+  "Predicted",
+  "No doubt",
+  "Definitely yes",
+  "You can be sure of that",
+  "I guess so, yes",
+  "Most likely",
+  "Good prospects",
+  "Signs say yes",
+  "Yes",
+  "It's not clear yet, try again",
+  "Ask later",
+  "It's better not to tell",
+  "You can't predict now",
+  "Concentrate and ask again",
+  "Don't even think about it",
+  "My answer is no",
+  "According to my data, no",
+  "The outlook is not good",
+  "Highly doubtful"
+]
+
+puts greetings.sample
+sleep 2
+puts answers.sample
